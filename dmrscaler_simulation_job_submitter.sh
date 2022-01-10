037e3aa98f43a3ff77b01ffa62ca980ff82032d0
Leroy Bondhus <leroybon@login1.hoffman2.idre.ucla.edu>
2022-01-09 21:30:28 -0800

    updates from remote cluster

--- dmrscaler_simulation_job_submitter.sh
+++ dmrscaler_simulation_job_submitter.sh
@@ -8,7 +8,7 @@
 #$ -l highp,h_rt=2:00:00,h_data=4G
 ## Modify the parallel environment
 ## and the number of cores as needed:
-#$ -pe shared 4
+#$ -pe shared 1
 # Email address to notify
 #$ -M $USER@mail
 # Notify when
@@ -27,7 +27,7 @@ echo " "
 module load R/4.1.0
 
 ## run setup to generate 1. Rdata that feeds into everything 2. simulation table 3. method table
-Rscript simul_setup.R
+#Rscript simul_setup.R
 
 NUM_SIMUL_SETS=`wc -l < simul_table.csv`
 NUM_METHOD_SETS=`wc -l < method_table.csv`
@@ -37,12 +37,12 @@ for SIMUL_SET_ID in {1..$NUM_SIMUL_SETS}
   do
   for METHOD_SET_ID in {1..$NUM_METHOD_SETS}
   do
+    echo "SIMUL SET: $SIMUL_SET_ID, METHOD_SET: $METHOD_SET_ID"
     qsub dmrscaler_simulation_job_individual.sh $SIMUL_SET_ID $METHOD_SET_ID
   done
 done
 
 
-Rscript simul_individual_run.R $SIMUL_SET_ID $METHOD_SET_ID
 
 
 # echo job info on joblog:

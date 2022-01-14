b3941f99b0cb508841d0d23cdb8aa4c5eb18f731
Leroy Bondhus <bondh185@ucla.edu>
2022-01-13 19:23:55 -0800

    testing pasted system call for combp

--- test_combp.R
+++ test_combp.R
@@ -9,12 +9,14 @@ output_dir <-paste("./results/")
 load("simul_setup.Rdata")
 
 
-
-
+out_file_prefix <- "./results/test_combp_out"
+in_file_name <- "./results/combp_temp_input.bed"
 combp_1 = list(method="combp",
-               function_call="system(\"comb-p pipeline -c 4 --dist 1000 --step 5000 \\
-       --seed 1e-3 -p ./test_combp_out \\
-       --region-filter-p 0.1 ./results/test_combp_input.bed \")")
+               function_call=
+                 paste("system(\"comb-p pipeline -c 4 --dist 1000 --step 500 ",
+                       " --seed 1e-3 --region-filter-p 0.1 -p ", out_file_prefix,
+                       " ",in_file_name,  " \") ", sep="" )
+               )
 
 method_set_result <- eval(parse(text=combp_1$function_call))
 
